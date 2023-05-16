f28738a6afa95b925e8f74aed980fcc8fadb3a38
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-05-16 14:03:45 -0700

    can see username on user card

--- client/src/components/UserCards.js
+++ client/src/components/UserCards.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./styles/userFeed.css";
 import img from "./testIMG/apple-music-note.jpg";
 import { QUERY_GET_PROFILE_CARDS } from "../utils/queries";
@@ -7,7 +7,9 @@ import { useQuery } from "@apollo/client";
 const Users = () => {
   const { loading, data } = useQuery(QUERY_GET_PROFILE_CARDS);
   const cards = data?.profileCards || [];
-
+useEffect(()=> {
+  console.log(cards)
+})
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -25,7 +27,7 @@ const Users = () => {
               />
             </div>
             <div className="user-info-container">
-              <h2 className="name">Username {user.username}</h2>
+              <h2 className="name">Username: {user.profile.username}</h2>
               <p className="info">Genres: {user.genres}</p>
               <p className="info">Instruments: {user.instrument}</p>
                <p className="info">Location: {user.location}</p>
