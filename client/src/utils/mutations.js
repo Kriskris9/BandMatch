7b8b8575e180147b83c59f3f714b307fa771d509
jjjgm <businessjgm@icloud.com>
2023-05-18 12:50:27 -0700

    username prob.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,5 @@
 import { gql } from "@apollo/client";
 
-
 export const ADD_PROFILE = gql`
   mutation addProfile($username: String!, $email: String!, $password: String!) {
     addProfile(username: $username, email: $email, password: $password) {
