6196b0c77b7cbdfa647eab493e4183e647afe8c6
jjjgm <businessjgm@icloud.com>
2023-05-18 19:39:45 -0700

    base / route

--- client/src/App.js
+++ client/src/App.js
@@ -74,7 +74,7 @@ function App() {
               <>
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
-                <Route path="/feed" element={<Feed />} />
+                <Route path="/" element={<Feed />} />
               </>
             )}
           </Routes>
