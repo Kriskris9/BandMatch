6fae5c35d5ab71d76822126d01eb995477072065
jjjgm <businessjgm@icloud.com>
2023-05-18 23:31:02 -0700

    link rerouting for auth purposes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
       <div className="header-container">
         <div>
           <h1>
-            <Link className="header-link" to="/feed">
+            <Link className="header-link" to="/bandmatch">
               BandMatch
             </Link>
           </h1>
